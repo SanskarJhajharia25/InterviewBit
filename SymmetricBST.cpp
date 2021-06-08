f9b2c8a73a7e30893c979c5f53f0c2629eef98ab
Sanskar Jhajharia <65020627+SanskarJhajharia25@users.noreply.github.com>
2021-06-08 18:23:30 +0530

    Create SymmetricBST.cpp

---
+++ SymmetricBST.cpp
@@ -0,0 +1,22 @@
+/* Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int comparer(TreeNode* A, TreeNode* B){
+    if(!A && !B) return 1;
+    if(!A || !B) return 0;
+    if(A->val != B->val) return 0;
+    
+    return (comparer(A->left, B->right) && comparer(A->right, B->left));
+} 
+
+int Solution::isSymmetric(TreeNode* A) {
+    return comparer(A,A);
+}
