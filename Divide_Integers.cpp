8f1561b17e0a43b344ebcea4f52c43620019e1f5
Sanskar Jhajharia <65020627+SanskarJhajharia25@users.noreply.github.com>
2021-06-19 02:05:25 +0530

    Create Divide_Integers.cpp

---
+++ Divide_Integers.cpp
@@ -0,0 +1,21 @@
+//https://www.interviewbit.com/problems/divide-integers/
+//Divide two integers without using multiplication, division and mod operator.
+
+//Return the floor of the result of the division.
+
+int Solution::divide(int a, int b) {
+    if(b==0)return INT_MAX ;
+    if(a==0)return 0 ;
+    if(b==1)return a ;
+    if(a==INT_MIN && b==-1)return 2147483647 ;
+int sign= (a<0)^(b<0)==0 ?1:-1 ;
+a=abs(a) ;
+b=abs(b) ;
+int ans=0 ;
+while(a-b>=0){
+ans++ ;
+a=a-b ;
+}
+return ans*sign ;
+
+}
