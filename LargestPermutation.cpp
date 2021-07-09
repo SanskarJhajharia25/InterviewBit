af5362b2eeaacbda1b5b018ae202e94a2a7e19b0
Sanskar Jhajharia <65020627+SanskarJhajharia25@users.noreply.github.com>
2021-07-09 13:46:00 +0530

    Create LargestPermutation.cpp

---
+++ LargestPermutation.cpp
@@ -0,0 +1,29 @@
+/*
+https://www.interviewbit.com/problems/largest-permutation/
+
+Given an integer array A of size N consisting of unique integers from 1 to N. You can swap any two integers atmost B times.
+
+Return the largest lexicographical value array that can be created by executing atmost B swaps.
+*/
+
+vector<int> Solution::solve(vector<int> &a, int B) {
+    int n=a.size();
+unordered_map<int,int>m;
+for(int i=0;i<n;i++){
+    m[a[i]]=i;
+}
+for(int i=0;i<n;i++){
+    if(B>0){
+        if(a[i]!=n-i){
+            B--;
+            m[a[i]]=m[n-i];
+            a[m[n-i]]=a[i];
+            m[n-i]=i;
+            a[i]=n-i;
+        }
+    }else{
+        break;
+    }
+}
+return a;
+}
